074b259c4bf6b0add287af855bb607c0396f494d
shozibahmedinfo <needshozib@gmail.com>
2025-02-07 20:49:13 +0600

    javascript switch on/off

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,7 @@
+// This is a javaScript function switch on/off
 function on(){
-    document.getElementById('by').src='on.gif';
+    document.getElementById('by').src='images/on.gif';
 };
 function off(){
-    document.getElementById('by').src='off.gif';
+    document.getElementById('by').src='images/off.gif';
 };
